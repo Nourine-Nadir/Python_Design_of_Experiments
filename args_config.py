f233dc6725ec3b014425825b7be4f5c66a5e4b69
Nourine-Nadir <nounadir@gmail.com>
2025-02-26 20:40:31 +0100

    Update

--- args_config.py
+++ args_config.py
@@ -17,42 +17,42 @@ PARSER_CONFIG = {
             'flags' : ('-anv', '--anova'),
             'help': 'Compute Anova and plot its table',
             'action' : 'store_true',
-            'default': False
+            'default': True
         },
 
     'maximize_optimization':{
         'flags' : ('-max', '--max_opt'),
         'help': 'Enable training models',
         'action' : 'store_true',
-        'default': False
+        'default': True
         },
 
     'main_visualizations':{
         'flags' : ('-main_viz', '--main_visualizations'),
         'help': 'Plot main Visualizations (pred_actual, normal_plot_of_res, res_pred, res_run)',
         'action' : 'store_true',
-        'default': False
+        'default': True
         },
 
     'box_cox':{
         'flags' : ('-box_cox', '--box_cox_visualizations'),
         'help': 'Compute and Plot box-cox visualization',
         'action' : 'store_true',
-        'default': False
+        'default': True
         },
 
     'response_contour_plot':{
         'flags' : ('-rc', '--response_contour_visualizations'),
         'help': 'Compute and Plot response contour visualization',
         'action' : 'store_true',
-        'default': False
+        'default': True
         },
 
     'response_surface_plot':{
         'flags' : ('-rsm', '--response_surface_visualizations'),
         'help': 'Compute and Plot response surface visualization',
         'action' : 'store_true',
-        'default': False
+        'default': True
         },
 
 }
